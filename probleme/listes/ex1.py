39361fa5dac2c697a92698ddf2c73737f229c37c
bilel233 <bilel.mjdb@gmail.com>
2024-07-04 13:24:38 +0200

    Intervertir le premier et dernier element d'une liste : 2eme version

--- probleme/listes/ex1.py
+++ probleme/listes/ex1.py
@@ -8,5 +8,17 @@ def intervertir(L):
     L[0] = L[n-1]
     L[n-1] = temp
     print(L)
+
+def intervertir1(L):
+    """
+    intervertit le premier et dernier element d'une liste L
+    """
+
+    L[0],L[-1] = L[-1],L[0]
+    print(f"la liste intervertit vaut {L}")
+
 if __name__ == "__main__":
     intervertir([12,35,9,56,24])
+    print()
+    intervertir1([12,35,9,56,24])
+
