bd73afb60d6147261b67a4c39c5ae91514d5e6d9
bilel233 <bilel.mjdb@gmail.com>
2024-07-28 18:42:50 +0100

    somme de deux entiers avec l'operateur "+"

---
+++ probleme/programmeBasique/ex1.py
@@ -0,0 +1,15 @@
+# ajouter deux nombres en python
+
+# avec l'operateur "+"
+
+a = 1
+b = 2
+
+s = a + b  # on somme les nombres
+
+
+
+print(f" a = {a} et b = {b}")   # on affiche les valeurs
+
+
+
