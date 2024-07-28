db034084e59d4fb0844c9b7c40a1f9981cb3adb7
bilel233 <bilel.mjdb@gmail.com>
2024-07-28 18:47:19 +0100

    somme de deux nombres a l'aide de l'operateur input

--- probleme/programmeBasique/ex1.py
+++ probleme/programmeBasique/ex1.py
@@ -11,5 +11,15 @@ s = a + b  # on somme les nombres
 
 print(f" a = {a} et b = {b}")   # on affiche les valeurs
 
+# ajouter deux nombres avec la fonction input
+
+a = int(input("premier nombre")) # saisit des nombres
+b = int(input("second nombre"))
+
+s = a + b   # somme des nombres
+
+print(f"s = {s}")   # affichage de la somme
+
+
 
 
