f5a169b9d1fd332a0188cdbd7c718448cac1b402
bilel233 <bilel.mjdb@gmail.com>
2024-07-29 10:14:29 +0100

    minimum à l'aide de la fonction min

--- probleme/programmeBasique/ex2.py
+++ probleme/programmeBasique/ex2.py
@@ -9,7 +9,11 @@ def minimum(a,b):
         return a
     return b
 
+# en utilisant la fonction min
+
+
 if __name__=="__main__":
     print(f"le minimum est {minimum(1,2)}")
     print(f"le minimum est {minimum(-1, 2)}")
     print(f"le minimum est {minimum(-1, -4)}")
+    print(f"le minimum en utilisant la fonction min est {min(10,9)}")
