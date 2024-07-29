0eb8d9e652bb3cf85bdc0a3f215ec1b041389597
bilel233 <bilel.mjdb@gmail.com>
2024-07-29 10:19:32 +0100

    minimum à l'aide de la fonction sorted

--- probleme/programmeBasique/ex2.py
+++ probleme/programmeBasique/ex2.py
@@ -17,3 +17,12 @@ if __name__=="__main__":
     print(f"le minimum est {minimum(-1, 2)}")
     print(f"le minimum est {minimum(-1, -4)}")
     print(f"le minimum en utilisant la fonction min est {min(10,9)}")
+
+    # en utilisant la methode sorted()
+
+    a = 12
+    b = 3
+    L = [a,b]
+    L1 = sorted(L)   # on trie la liste de facon croissante
+    print(f"la valeur minimale est {L1[0]} ")
+
