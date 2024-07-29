79927faadcbeb0efdf6c890fec3f83033d1066da
bilel233 <bilel.mjdb@gmail.com>
2024-07-29 16:56:03 +0100

    insertion d'un noeud au debut de la liste chainee circulaire

--- listesChaineeCirculaire/ex1.py
+++ listesChaineeCirculaire/ex1.py
@@ -4,17 +4,18 @@
 class Node:
     """representation d'un noeud d'une liste chainee circulaire"""
 
-    def __init__(self,val):
+    def __init__(self, val):
         self.val = val
         self.next = None
+
 
 class ListeChaineeCirculaire:
     """modelisation de la liste chainee circulaire"""
 
     def __init__(self):
-        self.last  = Node
+        self.last = None
 
-    def insertionListeVide(self,val):
+    def insertionListeVide(self, val):
         """Insert un noeud dans la liste chainee circulaire"""
 
         if self.last is not None:
@@ -23,13 +24,23 @@ class ListeChaineeCirculaire:
         n = Node(val)
         self.last = n
         self.last.next = self.last
+        #print(self.last.val)
         return self.last
+
+    def insertionDebutListeChaineeCirculaire(self, val):
+        """Insere un noeud au debut de la liste chainee circulaire"""
+
+        if self.last is None:
+            self.insertionListeVide(val)
+        n = Node(val)
+        n.next = self.last.next
+        self.last = n
+
+
 if __name__ == "__main__":
-
     L = ListeChaineeCirculaire()
-    L.insertionListeVide(10)
-
-
-
-
-
+    L.insertionDebutListeChaineeCirculaire(100)
+    print(L.last.val)
+    L.insertionDebutListeChaineeCirculaire(101)
+    print(L.last.val)
+    print(L.last.next.val)
