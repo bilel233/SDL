a7a926ce4cb9cb820ed345f2a76cbcc7579631fa
bilel233 <bilel.mjdb@gmail.com>
2024-07-29 10:53:38 +0100

    pgcd de deux entiers en utilisant la fonction recursive

--- probleme/programmeBasique/ex3.py
+++ probleme/programmeBasique/ex3.py
@@ -7,3 +7,15 @@ pgcd = gcd(3,2)
 print(f"le pgcd entre 3 et 2 est {pgcd}")
 print(f"le pgcd entre 6 et 4 est {gcd(6,4)}")
 print(f"le pgcd entre 60 et 48 est {gcd(60,48)}")
+
+# en utilisant la methode recursif
+
+def recusivePgcd(a,b):
+    """renvoie le pgc de deux entiers"""
+
+    if b == 0:
+        return a
+    return recusivePgcd(b,a%b)
+
+if __name__ == "__main__":
+    print(f"le pgcd entre a et b vaut {recusivePgcd(60,48)}")
