3707158fce9c30c25c9460b0248c9e1f857d9c8e
bilel233 <bilel.mjdb@gmail.com>
2024-07-29 10:43:18 +0100

    pgcd de deux entiers en utilisant la fonction gcd

---
+++ probleme/programmeBasique/ex3.py
@@ -0,0 +1,9 @@
+# programme python pour calculer le pgcd de deux nombres
+
+
+from math import *
+
+pgcd = gcd(3,2)
+print(f"le pgcd entre 3 et 2 est {pgcd}")
+print(f"le pgcd entre 6 et 4 est {gcd(6,4)}")
+print(f"le pgcd entre 60 et 48 est {gcd(60,48)}")
