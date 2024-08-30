10466470a15af17ad8e40a9ef882366193ad524c
bilel233 <bilel.mjdb@gmail.com>
2024-08-30 11:06:02 +0200

    seconde version du programme somme

--- probleme/programmeBasique/ex4.py
+++ probleme/programmeBasique/ex4.py
@@ -8,5 +8,13 @@ sum = a + b     # on realise la somme des deux nombres
 print(f"la somme des deux nombres vaut : {sum}")
 
 
+# utilisation de l'operateur input
 
+n1 = input("veuillez saisir un nombre") # saisit du premier nombre
+n2 = input("veuillez saisir un nombre") # saisit du second nombre
+
+som = float(n1) + float(n2) # somme des entiers
+
+
+print(f"la somme vaut {som}")
 
