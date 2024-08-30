077dff1946a441c9fc6d425f5ac2cfb938968dba
bilel233 <bilel.mjdb@gmail.com>
2024-08-30 11:01:12 +0200

    premiere version du programme somme

---
+++ probleme/programmeBasique/ex4.py
@@ -0,0 +1,12 @@
+# additionner deux nombres avec l'operateur +
+
+a = 15
+b = 12
+
+sum = a + b     # on realise la somme des deux nombres
+
+print(f"la somme des deux nombres vaut : {sum}")
+
+
+
+
